4855a80621397722927bad616027bf65deba104b
Arjun <arjungravi1@gmail.com>
2024-01-29 21:06:26 +0530

    Committed

---
+++ 01_Attention is all you need/_Implementation.py
@@ -0,0 +1,119 @@
+# Taken from https://www.youtube.com/watch?v=U0s0f995w14
+
+import torch
+import torch.nn as nn
+
+
+class SelfAttention(nn.Module):
+    def __init__(self, embed_size, heads):
+        super(SelfAttention, self).__init__()
+        self.embed_size = embed_size
+        self.heads = heads
+        self.head_dim = embed_size // heads
+        
+        assert (
+            self.head_dim * heads == embed_size
+        ), "Embedding size needs to be divisible by heads" 
+
+        self.values = nn.Linear(embed_size, embed_size)
+        self.keys = nn.Linear(embed_size, embed_size)
+        self.queries = nn.Linear(embed_size, embed_size)
+        self.fc_out = nn.Linear(embed_size, embed_size)
+        
+    def forward(self, values, keys, query, mask):
+        N = query.shape[0]
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+
+        values = self.values(values)
+        keys = self.keys(keys)
+        queries = self.queries(query)
+        
+        values = values.reshape(N, value_len, self.heads, self.head_dim)
+        keys = keys.reshape(N, key_len, self.heads, self.head_dim)
+        queries = queries.reshape(N, query_len, self.heads, self.head_dim) 
+        
+        
+        energy = torch.einsum("nqhd,nkhd->nhqk", [queries, keys]) 
+
+        ''' In the context of the self-attention mechanism, "energy" refers to the 
+        unnormalized attention scores computed for each element in the sequence.
+        The term "energy" is often used as a metaphor to represent how much attention
+        or focus should be given to a particular element.
+        '''
+        
+        
+        if mask is not None:
+            energy = energy.masked_fill(mask == 0, float("-1e20"))
+        
+        
+        attention = torch.softmax(energy / (self.embed_size ** (1 / 2)), dim=3)
+        
+        out = torch.einsum("nhql,nlhd->nqhd", [attention, values]).reshape(
+            N, query_len, self.heads * self.head_dim
+        )
+        out = self.fc_out(out)
+        return out
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+
+
