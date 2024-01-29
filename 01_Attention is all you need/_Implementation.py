6a72b8f7f21961a128126f8adeec1b2920791046
Arjun <arjungravi1@gmail.com>
2024-01-30 05:28:33 +0530

    Committed

--- 01_Attention is all you need/_Implementation.py
+++ 01_Attention is all you need/_Implementation.py
@@ -58,7 +58,30 @@ class SelfAttention(nn.Module):
         
         
         
-        
+class TransformerBlock(nn.Module):
+    ''' Refers to the upper part of the encoder block, on the left side of the transformer.'''
+    def __init__(self, embed_size, heads, dropout, forward_expansion):
+        super(TransformerBlock, self).__init__()
+        self.attention = SelfAttention(embed_size, heads)
+        self.norm1 = nn.LayerNorm(embed_size)
+        self.norm2 = nn.LayerNorm(embed_size)
+
+        self.feed_forward = nn.Sequential(
+            nn.Linear(embed_size, forward_expansion * embed_size),
+            nn.ReLU(),
+            nn.Linear(forward_expansion * embed_size, embed_size),
+        )
+
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, value, key, query, mask):
+        attention = self.attention(value, key, query, mask)
+
+        # x is for the resnet connection
+        x = self.dropout(self.norm1(attention + query))
+        forward = self.feed_forward(x)
+        out = self.dropout(self.norm2(forward + x))
+        return out        
         
         
         
